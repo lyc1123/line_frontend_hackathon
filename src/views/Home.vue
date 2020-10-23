1253ecca0547786ead85507cfd679775d8c5f66c
LYC <lyc891123@gmail.com>
2020-10-23 17:49:45 +0800

    css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -258,9 +258,21 @@ p{
 input,select{
     font-size: 5vw;
     width: 88%;
+    border: 1px solid #dfdfdf;
 }
-select{
-  background-color: white;
+
+select {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  background: transparent;
+  background-image: url("data:image/svg+xml;utf8,<svg fill='black' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>");
+  background-repeat: no-repeat;
+  background-position-x: 100%;
+  background-position-y: -2px;
+  border-radius: 2px;
+  margin-right: 2rem;
+  padding: 0.1rem;
+  padding-right: 2rem;
 }
 input[type='submit']{
     width: 20vw;
